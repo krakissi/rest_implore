177c9146c95382535c977cb43edcf09609ef14e6
Mike Perron <mperron@extremenetworks.com>
2016-10-27 12:00:37 -0400

    Add rest_request wrapper script
    Add the convenient bash script which pipes rest_implore into netcat to
    make HTTP calls.

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-all: rest_implore
+all: rest_implore rest_request
 
 rest_implore: rest_implore.cc
 	@g++ -std=c++11 -s rest_implore.cc -o rest_implore
+
+rest_request: rest_request.bash
+	@cp rest_request.bash rest_request
+	@chmod a+x rest_request
---
+++ rest_request.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+export server=${server:-localhost}
+export port=${port:-8080}
+
+# Use the installed version if present, else attempt to use the local bin.
+implore=$(which rest_implore)
+
+if [ -z "$implore" ] || ! [ -x "$implore" ]; then
+    implore="./rest_implore"
+fi
+
+if ! [ -x "$implore" ]; then
+    echo "FATAL: Could not locate rest_implore binary (or it was not executable)."
+    exit 1
+fi
+
+export implore
+
+# Verify netcat is installed/executable.
+if [ -z "$(which nc)" ]; then
+    echo "FATAL: netcat (nc) is not available."
+    exit 2
+fi
+
+$implore <&0 | nc "$server" "$port"
+
+echo ""
